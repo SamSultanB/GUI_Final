df5a1842d6fae614db570a4c9bf25f07b13c632b
Lux <ssbekmuratov@gmail.com>
2022-05-29 22:49:32 +0600

    last

--- main.py
+++ main.py
@@ -12,28 +12,28 @@ class GUI:
         self.tk = tk
 
     def main(self):
-        self.rotate = Label(self.tk, text='Rotate: ', font=("ariel 17 bold"))
+        self.rotate = Label(self.tk, text='Rotate: ', bg='PINK', font=("ariel 17 bold"))
         self.rotate.place(x=630, y=65)
         angles = [0, 45, 90, 180, 270, 360]
         self.rotation = ttk.Combobox(self.tk, width=13, values=angles, font=('italic'))
         self.rotation.place(x=725, y=67)
         self.rotation.bind("<<ComboboxSelected>>", self.__rotatePhoto)
 
-        self.waterMark = Label(self.tk, text='WaterMark: ', font=("ariel 17 bold"))
+        self.waterMark = Label(self.tk, text='WaterMark: ', bg='PINK', font=("ariel 17 bold"))
         self.waterMark.place(x=245, y=65)
         self.entry_text = Entry(self.tk, width=25)
-        self.entry_text.place(x=380, y=73)
+        self.entry_text.place(x=385, y=73)
         self.button1 = Button(text='Print', font=('underline'), relief=RAISED, command=self.__waterMark)
-        self.button1.place(x=535, y=65)
+        self.button1.place(x=540, y=65)
 
-        self.resize = Label(self.tk, text='Size:', font=("ariel 17 bold"))
+        self.resize = Label(self.tk, text='Size:', bg='PINK', font=("ariel 17 bold"))
         self.resize.place(x=630, y=10)
         sizes = ['1920x1080', '1280x720', '1080x1080', '720x720', '640x360']
         self.resizing = ttk.Combobox(tk, width=13, values=sizes, font=('italic'))
         self.resizing.bind("<<ComboboxSelected>>", self.__resizePhoto)
         self.resizing.place(x=725, y=14)
 
-        self.filter = Label(self.tk, text="Filter:", font=("ariel 17 bold"))
+        self.filter = Label(self.tk, text="Filter:", bg='PINK', font=("ariel 17 bold"))
         self.filter.place(x=245, y=10)
         f = ['BLUR', 'CONTOUR', 'DETAIL', 'EMBOSS', 'EDGE_ENHANCE_MORE', 'FIND_EDGES', 'SHARPEN', 'SMOOTH']
         self.filters = ttk.Combobox(self.tk, width=23, values=f, font=("italic"))
@@ -43,15 +43,18 @@ class GUI:
         self.select = Button(self.tk, text='Select image', width=23, bg='SILVER', fg='BLUE', font=('underline'), relief=RAISED, command=self.__select)
         self.select.place(x=5, y=240)
 
-        self.entryName = Label(self.tk, text='Save as: ', font=("ariel 12 bold"))
-        self.entryName.place(x=1, y=297)
-        self.entryFileName = Entry(self.tk, width=25)
-        self.entryFileName.place(x=71, y=300)
+        self.entryName = Label(self.tk, text='Save as: ',bg='BLACK', fg='WHITE', font=("ariel 12 bold"))
+        self.entryName.place(x=5, y=297)
+        self.entryFileName = Entry(self.tk, width=22)
+        self.entryFileName.place(x=82, y=300)
         self.save = Button(self.tk, text='Save', width=23, bg='SILVER', fg='BLUE', font=('underline'), relief=RAISED, command=self.__save)
         self.save.place(x=5, y=320)
 
         self.exit = Button(self.tk, text='Exit', width=23, bg='SILVER', fg='BLUE', font=('underline'), relief=RAISED, command=self.tk.destroy)
         self.exit.place(x=5, y=370)
+
+        self.about = Button(self.tk, text='About', width=15, bg='SILVER', fg='BLUE', font=('underline'), relief=RAISED, command=self.__about)
+        self.about.place(x=5, y=650)
 
         img = ImageTk.PhotoImage(Image.open("font/logo.jpg"))
         self.logo = Label(self.tk, image=img)
@@ -101,6 +104,8 @@ class GUI:
         complete_name = os.path.join('./images', filename+'.png')
         self.image.save(complete_name)
 
+    def __about(self):
+        pass
 
 tk = Tk()
 tk.geometry("900x700")
